1ccf8e97d4b7371dd3102d6bc6cded853e5b687b
Shuhey <shuhey.dev@gmail.com>
2020-05-30 01:24:57 +0900

    Add project files.

---
+++ XFNotificationUseLib/XFNotificationUseLib/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Plugin.LocalNotification;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFNotificationUseLib
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            NotificationCenter.Current.NotificationTapped += Current_NotificationTapped;
+
+            MainPage = new MainPage();
+        }
+
+        private void Current_NotificationTapped(NotificationTappedEventArgs e)
+        {
+            var lbl = new Label
+            {
+                Text = "tapped"
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XFNotificationUseLib/XFNotificationUseLib/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.LocalNotifications;
+using Plugin.LocalNotification;
+
+namespace XFNotificationUseLib
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int counter = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            counter++;
+            //CrossLocalNotifications.Current.Show($"Test{counter}", "This is Test");
+
+            var notification = new NotificationRequest
+            {
+                NotificationId = 100,
+                Title = "Test",
+                Description = $"This is Test{counter} from Plugin.LocalNotification",
+                ReturningData = "Dammy data.",
+                NotifyTime = DateTime.Now.AddSeconds(2),
+                BadgeNumber = 3
+            };
+
+            notification.Android.IconName = "icon.jpg";
+
+            
+
+            NotificationCenter.Current.Show(notification);
+        }
+    }
+}
